d5882e332cdf13c5713d734d76f4bc00a691f1d4
Quang <159697999+progressive-newbie263@users.noreply.github.com>
2024-12-21 14:20:45 +0700

    first commit
    Fullstack project for creating a YT clone, customized UI. Learning how to use material UI (MUI) and using API more efficiently

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import {
+  SearchFeed,
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail
+} from './components';
+
+
+/*
+ design:
+  + Thanh Navbar tren cung 
+  + Duoi thanh navbar, phan noi dung/ main thi se chia thanh cac component trong:
+*/
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000'}}>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Feed } from './Feed.jsx';
+export { default as ChannelDetail } from './ChannelDetail.jsx';
+export { default as SearchFeed } from './SearchFeed.jsx';
+export { default as VideoDetail } from './VideoDetail.jsx';
+
+export { default as Navbar } from './Navbar.jsx';
